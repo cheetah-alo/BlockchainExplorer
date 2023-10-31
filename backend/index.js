7a3380c2eef15104736b42a3b7b28264eb3b5eef
jackybarraza <jackybarraza@gmail.com>
2023-10-31 19:47:08 +0100

    adding infura api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT;
+const URL_INFURA = process.env.URL_INFURA;
 
 app.get("/ping", async (req, res) => {
   res.send({ fecha: new Date().toISOString() });
