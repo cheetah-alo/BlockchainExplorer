01086ce7eea8e34518068e305b4da1b8bd3e9909
jackybarraza <jackybarraza@gmail.com>
2023-11-05 20:08:06 +0100

    create a file api and rebuild the other components

--- frontend/src/components/Block.jsx
+++ frontend/src/components/Block.jsx
@@ -2,14 +2,7 @@ import { Outlet, useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import ReactJson from "react-json-pretty";
 import "react-json-pretty/themes/monikai.css"; // Import a theme for styling (you can choose a different theme)
-
-async function getBlock(block) {
-  const response = await fetch(
-    `http://localhost:2525/block/${block.queryKey[1]}`
-  );
-  const data = await response.json();
-  return data;
-}
+import { getBlock } from "../api";
 
 export function Block() {
   const params = useParams();
@@ -18,8 +11,8 @@ export function Block() {
     getBlock
   );
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (isError) return <h1>There is an error...</h1>;
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError) return <h2>There is an error...</h2>;
 
   return (
     <div className="container">
