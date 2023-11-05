edcbe98da8abe8cbd79857879aa2362dd5f68d47
jackybarraza <jackybarraza@gmail.com>
2023-11-05 19:17:09 +0100

    improving code redeability and handle errors

--- backend/utils.js
+++ backend/utils.js
@@ -1,8 +1,15 @@
+/**
+ * Serializes a JavaScript object, converting BigInt values to strings.
+ * @param {string} key - The key of the object property.
+ * @param {*} value - The value of the object property.
+ * @returns {*} - The serialized value, with BigInt values converted to strings.
+ */
 function serializeBigInt(key, value) {
   if (typeof value === "bigint") {
+    // Convert BigInt values to strings to ensure compatibility with JSON
     return value.toString();
   }
-  return value;
+  return value; // Return the value unchanged for non-BigInt properties
 }
 
 module.exports = { serializeBigInt };
